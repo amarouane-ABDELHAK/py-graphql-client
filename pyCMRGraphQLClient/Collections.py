8f0682f6bca881d2618f089fc6e049d12f1294ca
amarouane-ABDELHAK <am0089@uah.edu>
2021-10-24 21:22:30 -0500

    Upgrade to 1.0.11

--- pyCMRGraphQLClient/Collections.py
+++ pyCMRGraphQLClient/Collections.py
@@ -17,7 +17,6 @@ class Collections(BaseClassClient):
     def append_service(self, subservice):
         if not self.items:
             self.items = BaseClassClient(service="items", fields=[])
-            self.query += self.items.get_query()
         self.items.query += subservice.get_query()
         self.query += self.items.get_query()
 
